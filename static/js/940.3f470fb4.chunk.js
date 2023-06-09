8719c53722d80c7e28cbeb8685258c5ba8e65dd7
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2023-06-09 21:08:40 +0000

    Deploying to gh-pages from @ RomanOdessa75/goit-react-hw-05-movies@16d29d7143a387a64ef9157a4c1ddf16d6d3e3b5 🚀

--- static/js/940.3f470fb4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[940],{940:function(e,t,n){n.r(t);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that movie"})]})}}}]);
-//# sourceMappingURL=940.3f470fb4.chunk.js.map
